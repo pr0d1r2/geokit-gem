885b649139f626e0f36b02aa30cb8415c68d4928
Marcin Nowicki <pr0d1r2@ragnarson.com>
2009-06-03 08:51:05 +0200

    Changed naming of one method to be precise.

--- lib/geokit/cached/geocodable.rb
+++ lib/geokit/cached/geocodable.rb
@@ -8,7 +8,7 @@ module Geokit
         end
       end
 
-      def geocoder
+      def multi_geocoder
         if cache_locations?
           Geokit::Geocoders::CachedMultiGeocoder
         else
@@ -17,7 +17,7 @@ module Geokit
       end
 
       def geocode_address_cached
-        @geocoder.geocode(complete_address)
+        multi_geocoder.geocode(complete_address)
         self.lat, self.lng, self.provider = @geo.lat, @geo.lng, @geo.provider if @geo.success
       end
 
